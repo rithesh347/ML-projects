19223d8322f5079e24695fc4a4ca660613b82b58
rithesh347 <23hr1a3045@gmail.com>
2025-12-30 11:27:05 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+class Arithmetic
+{
+    public static void main(String args[])
+    {
+int a=20;
+int b=12;
+System.out.println("addition of a+b;"+(a+b));
+System.out.println("substraction of a-b;"+(a-b));
+System.out.println("multiplication of a*b;"+(a*b));
+System.out.println("division of a/b ;"+(a/b));
+System.out.println("modulus of a%b;"+(a%b));
+}
+} 
